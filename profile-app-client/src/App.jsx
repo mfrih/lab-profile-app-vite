f053d846ddbe7e047f10c01fc69b709ba0d2bfe2
Maria Frih <maria.frih@gmail.com>
2024-02-07 09:59:59 +0100

    adds Login Page component and targets the server login route

--- profile-app-client/src/App.jsx
+++ profile-app-client/src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/signup" />
-        <Route path="/login" /> */}
+        {/* <Route path="/signup" /> */}
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   );
---
+++ profile-app-client/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+const API_URL = 'http://localhost:5005';
+
+const LoginPage = () => {
+  const [formState, setFormState] = useState({
+    username: '',
+    password: '',
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const key = e.target.id;
+    const value = e.target.value;
+    setFormState({ ...formState, [key]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${API_URL}/auth/login`, formState);
+      const authToken = response.data.authToken;
+      localStorage.setItem('authToken', authToken);
+      navigate('/');
+    } catch (error) {
+      console.error('Error logging in', error);
+    }
+  };
+  return (
+    <div>
+      <h2>Log In</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" onChange={handleChange} />
+        </div>
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
